9e8fb7d619bf469c07a1b0a69d954bf275cc0175
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-06-06 06:15:48 -0300

    day 2

--- pages/html-form/script.js
+++ pages/html-form/script.js
@@ -1,17 +1,51 @@
-const elemForm = document.getElementById('form')
-const elemName = document.getElementById('nome')
-const elemBirthDate = document.getElementById('birth-date')
+const elemForm = document.getElementById("form");
+const elemName = document.getElementById("nome");
+const elemBirthDate = document.getElementById("birth-date");
 
-function checkRequired(inputArr){
-  inputArr.forEach(elem => {
-      console.log(elem.value);
-    });
+function showError(input, message) {
+  const elemFormControl = input.parentElement;
+  elemFormControl.className = "form-control error";
+  const elemSmall = elemFormControl.querySelector("small");
+  elemSmall.innerText = message;
 }
 
-elemForm.addEventListener('submit', function(e){
-  e.preventDefault()
+function showSuccess(input) {
+  const elemFormControl = input.parentElement;
+  elemFormControl.className = "form-control success";
+}
 
+function checkRequired(inputArr) {
+  inputArr.forEach((elem) => {
+    if (elem.value.trim() === "") {
+      showError(elem, ` Campo obrigtório`);
+    } else {
+      showSuccess(elem);
+    }
+  });
+}
 
-  checkRequired([elemName, elemBirthDate])
-})
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `Campo deve ter mais  ${min} caracteres`);
+  } else if (input.value.length > max) {
+    showError(input, `Campo nao pode ser maior que  ${max} caracteres`);
+  } else {
+    showSuccess(input);
+  }
+}
 
+elemName.onblur = () => {
+  checkRequired([elemName, elemBirthDate]);
+  checkLength(elemName, 3, 10);
+};
+elemBirthDate.onblur = () => {
+  checkRequired([elemName, elemBirthDate]);
+};
+
+elemForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkRequired([elemName, elemBirthDate]);
+
+  checkLength(elemName, 3, 10);
+});
