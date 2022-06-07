83dfd7aa9d426d6487ce4fac8871310f7a947188
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-06-07 15:22:46 -0300

    add funcions add an btn

--- pages/html-form/script.js
+++ pages/html-form/script.js
@@ -2,30 +2,7 @@ const elemForm = document.getElementById("form");
 const elemName = document.getElementById("nome");
 const elemBirthDate = document.getElementById("birth-date");
 const elemTblBirth = document.getElementById("tblBirth");
-
-const listBirth = [
-    { name: "Isadora", dtBirth: "2018/02/04" },
-    { name: "Emily", dtBirth: "2018/02/04" },
-];
-
-const list = elemTblBirth.innerHTML;
-console.log(list);
-
-listBirth.forEach((elem, key) => {
-    let newList =
-        list +
-        `
-  <tr>
-    <td>${elem.name}</td>
-    <td>${elem.dtBirth}</td>
-  <td>
-      <button>Editar</button>
-      <button>Remover</button>
-  </td>
-</tr>`;
-
-    elemTblBirth.innerHTML = newList;
-});
+const elemBtnSubmit = document.getElementById("btn-submit");
 
 function showError(input, message) {
     const elemFormControl = input.parentElement;
@@ -37,6 +14,32 @@ function showError(input, message) {
 function showSuccess(input) {
     const elemFormControl = input.parentElement;
     elemFormControl.className = "form-control success";
+}
+
+function removeSuccess(inputArray) {
+    inputArray.forEach((elem) => {
+        const elemFormControl = elem.parentElement;
+        elemFormControl.className = "form-control";
+    });
+}
+
+function enableBtn(inputArray) {
+    const qtdFild = inputArray.length;
+    let qtdSuccess = 0;
+    inputArray.forEach((elem) => {
+        const elemFormControl = elem.parentElement;
+        if (elemFormControl.className === "form-control success") {
+            qtdSuccess++;
+        }
+    });
+
+    if (qtdFild === qtdSuccess) {
+        elemBtnSubmit.removeAttribute("disabled");
+        elemBtnSubmit.className = "btn enable";
+    } else {
+        elemBtnSubmit.setAttribute("disabled", "");
+        elemBtnSubmit.className = "btn";
+    }
 }
 
 function checkRequired(inputArr) {
@@ -59,12 +62,49 @@ function checkLength(input, min, max) {
     }
 }
 
+function addBirth() {
+    const list = elemTblBirth.innerHTML;
+
+    const listBirth = [];
+
+    const birthdayPerson = {
+        name: elemName.value,
+        dtBirth: elemBirthDate.value,
+    };
+
+    listBirth.push(birthdayPerson);
+
+    elemName.value = "";
+    elemBirthDate.value = "";
+
+    removeSuccess([elemName, elemBirthDate]);
+
+    elemBtnSubmit.setAttribute("disabled", "");
+    elemBtnSubmit.className = "btn";
+
+    listBirth.forEach((elem, key) => {
+        let newList =
+            list +
+            `<tr>
+                <td>${elem.name}</td>
+                <td>${elem.dtBirth}</td>
+                <td>
+                    <button>Editar</button>
+                    <button>Remover</button>
+                </td>
+            </tr>`;
+        elemTblBirth.innerHTML = newList;
+    });
+}
+
 elemName.onblur = () => {
     checkRequired([elemName, elemBirthDate]);
     checkLength(elemName, 3, 10);
+    enableBtn([elemName, elemBirthDate]);
 };
 elemBirthDate.onblur = () => {
     checkRequired([elemName, elemBirthDate]);
+    enableBtn([elemName, elemBirthDate]);
 };
 
 elemForm.addEventListener("submit", function(e) {
@@ -73,4 +113,6 @@ elemForm.addEventListener("submit", function(e) {
     checkRequired([elemName, elemBirthDate]);
 
     checkLength(elemName, 3, 10);
+
+    addBirth();
 });
