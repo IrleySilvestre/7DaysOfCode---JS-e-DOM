55ec9f6bf174b3efc87d3222744ddb97eeb49457
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-06-09 06:17:07 -0300

    day 1

--- pages/html-form/script.js
+++ pages/html-form/script.js
@@ -2,117 +2,142 @@ const elemForm = document.getElementById("form");
 const elemName = document.getElementById("nome");
 const elemBirthDate = document.getElementById("birth-date");
 const elemTblBirth = document.getElementById("tblBirth");
+const elemTblEdite = document.getElementById("btn-edite");
 const elemBtnSubmit = document.getElementById("btn-submit");
 
 function showError(input, message) {
-    const elemFormControl = input.parentElement;
-    elemFormControl.className = "form-control error";
-    const elemSmall = elemFormControl.querySelector("small");
-    elemSmall.innerText = message;
+  const elemFormControl = input.parentElement;
+  elemFormControl.className = "form-control error";
+  const elemSmall = elemFormControl.querySelector("small");
+  elemSmall.innerText = message;
 }
 
 function showSuccess(input) {
-    const elemFormControl = input.parentElement;
-    elemFormControl.className = "form-control success";
+  const elemFormControl = input.parentElement;
+  elemFormControl.className = "form-control success";
 }
 
 function removeSuccess(inputArray) {
-    inputArray.forEach((elem) => {
-        const elemFormControl = elem.parentElement;
-        elemFormControl.className = "form-control";
-    });
+  inputArray.forEach((elem) => {
+    const elemFormControl = elem.parentElement;
+    elemFormControl.className = "form-control";
+  });
 }
 
 function enableBtn(inputArray) {
-    const qtdFild = inputArray.length;
-    let qtdSuccess = 0;
-    inputArray.forEach((elem) => {
-        const elemFormControl = elem.parentElement;
-        if (elemFormControl.className === "form-control success") {
-            qtdSuccess++;
-        }
-    });
-
-    if (qtdFild === qtdSuccess) {
-        elemBtnSubmit.removeAttribute("disabled");
-        elemBtnSubmit.className = "btn enable";
-    } else {
-        elemBtnSubmit.setAttribute("disabled", "");
-        elemBtnSubmit.className = "btn";
+  const qtdFild = inputArray.length;
+  let qtdSuccess = 0;
+  inputArray.forEach((elem) => {
+    const elemFormControl = elem.parentElement;
+    if (elemFormControl.className === "form-control success") {
+      qtdSuccess++;
     }
+  });
+
+  if (qtdFild === qtdSuccess) {
+    elemBtnSubmit.removeAttribute("disabled");
+    elemBtnSubmit.className = "btn enable";
+  } else {
+    elemBtnSubmit.setAttribute("disabled", "");
+    elemBtnSubmit.className = "btn";
+  }
 }
 
 function checkRequired(inputArr) {
-    inputArr.forEach((elem) => {
-        if (elem.value.trim() === "") {
-            showError(elem, ` Campo obrigtório`);
-        } else {
-            showSuccess(elem);
-        }
-    });
+  inputArr.forEach((elem) => {
+    if (elem.value.trim() === "") {
+      showError(elem, ` Campo obrigtório`);
+    } else {
+      showSuccess(elem);
+    }
+  });
 }
 
 function checkLength(input, min, max) {
-    if (input.value.length < min) {
-        showError(input, `Campo deve ter mais  ${min} caracteres`);
-    } else if (input.value.length > max) {
-        showError(input, `Campo nao pode ser maior que  ${max} caracteres`);
-    } else {
-        showSuccess(input);
-    }
+  if (input.value.length < min) {
+    showError(input, `Campo deve ter mais  ${min} caracteres`);
+  } else if (input.value.length > max) {
+    showError(input, `Campo nao pode ser maior que  ${max} caracteres`);
+  } else {
+    showSuccess(input);
+  }
 }
 
 function addBirth() {
-    const list = elemTblBirth.innerHTML;
+  const list = elemTblBirth.innerHTML;
 
-    const listBirth = [];
+  const listBirth = [];
 
-    const birthdayPerson = {
-        name: elemName.value,
-        dtBirth: elemBirthDate.value,
-    };
+  const data = new Date(elemBirthDate.value);
 
-    listBirth.push(birthdayPerson);
+  const dt = `${data.getUTCDate().toString().padStart(2, "0")}/${(
+    data.getUTCMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}/${data.getUTCFullYear()}`;
 
-    elemName.value = "";
-    elemBirthDate.value = "";
+  const birthdayPerson = {
+    name: elemName.value,
+    dtBirth: dt,
+  };
 
-    removeSuccess([elemName, elemBirthDate]);
+  listBirth.push(birthdayPerson);
 
-    elemBtnSubmit.setAttribute("disabled", "");
-    elemBtnSubmit.className = "btn";
+  elemName.value = "";
+  elemBirthDate.value = "";
 
-    listBirth.forEach((elem, key) => {
-        let newList =
-            list +
-            `<tr>
+  removeSuccess([elemName, elemBirthDate]);
+
+  elemBtnSubmit.setAttribute("disabled", "");
+  elemBtnSubmit.className = "btn";
+
+  listBirth.forEach((elem) => {
+    let newList =
+      list +
+      `<tr>
                 <td>${elem.name}</td>
                 <td>${elem.dtBirth}</td>
                 <td>
-                    <button>Editar</button>
-                    <button>Remover</button>
+                    <button id="btn-edite" >Editar</button>
+                    <button id="btn-remove" >Remover</button>
                 </td>
             </tr>`;
-        elemTblBirth.innerHTML = newList;
-    });
+    elemTblBirth.innerHTML = newList;
+  });
+
+  addEventBtn();
+}
+
+function addEventBtn() {
+  const td = document.querySelectorAll("#btn-edite");
+
+  td.forEach((elem) => {
+    elem.addEventListener("click", edite);
+  });
+}
+
+function edite(e) {
+  const tr = e.target.parentElement.parentElement.children;
+  elemName.value = tr[0].innerHTML;
+  elemBirthDate.value = tr[1].innerHTML;
 }
 
 elemName.onblur = () => {
-    checkRequired([elemName, elemBirthDate]);
-    checkLength(elemName, 3, 10);
-    enableBtn([elemName, elemBirthDate]);
+  checkRequired([elemName, elemBirthDate]);
+  checkLength(elemName, 3, 10);
+  enableBtn([elemName, elemBirthDate]);
 };
 elemBirthDate.onblur = () => {
-    checkRequired([elemName, elemBirthDate]);
-    enableBtn([elemName, elemBirthDate]);
+  checkRequired([elemName, elemBirthDate]);
+  enableBtn([elemName, elemBirthDate]);
 };
 
-elemForm.addEventListener("submit", function(e) {
-    e.preventDefault();
+elemForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    checkRequired([elemName, elemBirthDate]);
+  checkRequired([elemName, elemBirthDate]);
 
-    checkLength(elemName, 3, 10);
+  checkLength(elemName, 3, 10);
 
-    addBirth();
+  addBirth();
 });
